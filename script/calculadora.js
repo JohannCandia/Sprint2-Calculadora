7ed60c8e68a7c028eeff6c4850ffda62ba59da1a
JohannCandia <jomacalla@gmail.com>
2023-04-21 22:40:44 -0400

    Funciones Extras
    Raiz y parentecis, se cambia el div de display por un text input para que no desaparezca al apretar CE

--- script/calculadora.js
+++ script/calculadora.js
@@ -1,47 +1,55 @@
 const pantalla = document.getElementById("pantalla");
 const botones = document.querySelectorAll("button");
 
-const agregaroperador = (operador)=>
-{
-    
-    if(pantalla.textContent.slice(-1).match(/[+\-*/]/))
-    {
-        pantalla.textContent=pantalla.textContent.slice(0,-1);
+const agregarOperador = (operador) => {
+  if (pantalla.value.slice(-1).match(/[+\-*/]/)) {
+    pantalla.value = pantalla.value.slice(0, -1);
+  }
+  pantalla.value += operador;
+};
+
+botones.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    const botonPulsado = boton.value;
+
+    if (boton.id === "igual") {
+      try {
+        const resultado = eval(pantalla.value);
+        pantalla.value = parseFloat(resultado.toFixed(3));
+      } catch (error) {
+        pantalla.value = "ERROR!";
+      }
+    } else if (boton.id === "borrar") {
+      pantalla.value = pantalla.value.slice(0, -1);
+    } else if (boton.id === "ce") {
+      pantalla.value = "";
+    } else if (boton.id === "raiz") {
+        pantalla.value = Math.sqrt(parseFloat(pantalla.value)).toFixed(1);
+      } else if (boton.textContent.match(/[+\-*/]/)) {
+      agregarOperador(boton.textContent);
+    } else {
+      pantalla.value = pantalla.value + botonPulsado;
     }
-    pantalla.textContent+=operador;
-}
+  });
+});
 
-botones.forEach((boton)=>{
-
-    boton.addEventListener("click",()=>{
-
-        const botonpulsado = boton.value;
-        pantalla.textContent=pantalla.textContent + botonpulsado;
-       
-        if(boton.id === "igual")
-        {
-            try{
-                const resultado = eval(pantalla.textContent);
-                pantalla.textContent=parseFloat(resultado.toFixed(3));
-            }
-            catch(error)
-            {
-                pantalla.textContent="ERROR!";
-            }
-        }
-        else if(boton.id==="borrar")
-        {
-            pantalla.textContent=pantalla.textContent.slice(0,-1); //0 es posicion inicial y -1 la cantidad que irá borrando de izq a der
-        }
-        else if(boton.id==="ce")
-        {
-            pantalla.textContent="";
-        }
+const botonesInput = (key) => {
+  if (key.match(/[+\-*/]/)) {
+    agregarOperador(key);
+  } else if (key.match(/[0-9]/)) {
+    pantalla.value = pantalla.value + key;
+  } else if (key === "Backspace") {
+    pantalla.value = pantalla.value.slice(0, -1);
+  } else if (key === "Enter" || key === "=") {
+    try {
+      const resultado = eval(pantalla.value);
+      pantalla.value = parseFloat(resultado.toFixed(3));
+    } catch (error) {
+      pantalla.value = "ERROR!";
     }
-    );   if (boton.textContent.match(/[+\-*/]/)) {
-        boton.addEventListener("click", () => agregaroperador(boton.textContent));
-        }}
-)
+  }
+};
 
-
-
+document.addEventListener("keydown", (event) => {
+  botonesInput(event.key);
+});
